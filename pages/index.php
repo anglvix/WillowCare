422611d54c3faf93e185e5aecc02481eaea07819
anglvix <roserainealt@gmail.com>
2026-05-18 15:58:37 +0100

    18/05

---
+++ pages/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'head.php';?>
+    <title>WillowCare</title>
+</head>
+<body>
+    <header>
+        <?php include 'navbar.php';?>
+    </header>
+
+    <main>
+        
+    </main>
+
+    <footer>
+        <?php include 'footer.php';?>
+    </footer>
+</body>
+</html>
