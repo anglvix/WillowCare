5ed3e4bb97bb2fa237b092fe2c81206510e4e4d9
anglvix <roserainealt@gmail.com>
2026-05-28 11:20:22 +0100

    28/05

--- router.php
+++ router.php
@@ -1,4 +1,6 @@
 <?php
+//se o url pedido for a raiz ou index.php, redireciona para a página principal
+
 if ($_SERVER['REQUEST_URI'] === '/' || $_SERVER['REQUEST_URI'] === '/index.php') {
     header('Location: /pages/index.php');
     exit;
