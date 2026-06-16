f505122476241f8d82605325c89df948a8ffd944
anglvix <roserainealt@gmail.com>
2026-06-17 00:07:01 +0100

    favicon

--- includes/head.php
+++ includes/head.php
@@ -19,6 +19,11 @@ $css_version = @filemtime(__DIR__ . '/../css/style.css') ?: time();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo htmlspecialchars($page_title, ENT_QUOTES); ?></title>
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $base_path; ?>images/favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $base_path; ?>images/favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $base_path; ?>images/favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo $base_path; ?>images/favicon_io/site.webmanifest">
+    <meta name="theme-color" content="#465E4B">
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- CSS personalizado -->
     <link rel="stylesheet" href="<?php echo $base_path; ?>css/style.css?v=<?php echo $css_version; ?>">
